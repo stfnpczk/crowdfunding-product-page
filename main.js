51cb7fe4fd4cb9e6df899962806297c41ef690ef
StefanP <stefandev_mail@pm.me>
2022-06-22 21:51:52 +0200

    Add styling for bookmarking

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+//mobile navigation modal
 const hamburger = document.getElementById("hamburger");
 const navModal = document.getElementById("mobile-nav");
-const overlay = document.getElementById("overlay")
+const overlay = document.getElementById("overlay");
 
 const toggleNavigation = () => {
   hamburger.classList.toggle("header__hamburger--active");
@@ -9,3 +10,17 @@ const toggleNavigation = () => {
 };
 
 hamburger.addEventListener("click", toggleNavigation);
+
+//bookmark functionality
+const bookmark = document.getElementById("bookmark");
+
+const toggleBookmark = () => {
+  bookmark.classList.toggle("intro__bookmark--set");
+  if (bookmark.classList.contains("intro__bookmark--set")) {
+    bookmark.querySelector("span").textContent = "Bookmarked";
+  } else {
+    bookmark.querySelector("span").textContent = "Bookmark";
+  }
+};
+
+bookmark.addEventListener("click", toggleBookmark);
