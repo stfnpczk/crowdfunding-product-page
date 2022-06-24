d2654532b21bdcdcf9bdb4f2a2950cea28953594
StefanP <stefandev_mail@pm.me>
2022-06-24 22:36:10 +0200

    Add display/close functionality to modal

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 const hamburger = document.getElementById("hamburger");
 const navModal = document.getElementById("mobile-nav");
 const overlay = document.getElementById("overlay");
+const body = document.querySelector("body")
 
 const toggleNavigation = () => {
   hamburger.classList.toggle("header__hamburger--active");
   navModal.classList.toggle("header__navigation--open");
   overlay.classList.toggle("overlay--active");
+  body.classList.toggle("body__overflow");
 };
 
 hamburger.addEventListener("click", toggleNavigation);
@@ -24,3 +26,30 @@ const toggleBookmark = () => {
 };
 
 bookmark.addEventListener("click", toggleBookmark);
+
+// display/close select modal
+modal = document.getElementById("modal");
+form = document.getElementById("form")
+selectButtons = document.querySelectorAll(".select");
+closeButton = document.getElementById("close-button")
+
+const displayModal = () => {
+  modal.classList.add("modal--show");
+  form.classList.add("modal__form--show");
+  body.classList.add("body__overflow");
+
+}
+selectButtons.forEach(button => {
+  button.addEventListener("click", displayModal);
+});
+
+
+const closeModal = () => {
+  modal.classList.remove("modal--show");
+  form.classList.remove("modal__form--show");
+  body.classList.remove("body__overflow");
+
+}
+closeButton.addEventListener("click", closeModal);
+
+
