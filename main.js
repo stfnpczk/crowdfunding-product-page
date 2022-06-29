b26a9aa4a668bcf6bc4e6c03675999cf5f7c3a45
StefanP <stefandev_mail@pm.me>
2022-06-29 21:47:46 +0200

    Add js logic to display pledge input

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const hamburger = document.getElementById("hamburger");
 const navModal = document.getElementById("mobile-nav");
 const overlay = document.getElementById("overlay");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
 
 const toggleNavigation = () => {
   hamburger.classList.toggle("header__hamburger--active");
@@ -29,27 +29,41 @@ bookmark.addEventListener("click", toggleBookmark);
 
 // display/close select modal
 modal = document.getElementById("modal");
-form = document.getElementById("form")
+form = document.getElementById("form");
 selectButtons = document.querySelectorAll(".select");
-closeButton = document.getElementById("close-button")
+closeButton = document.getElementById("close-button");
 
+
+//?? DRY 
 const displayModal = () => {
   modal.classList.add("modal--show");
   form.classList.add("modal__form--show");
   body.classList.add("body__overflow");
-
-}
-selectButtons.forEach(button => {
+};
+selectButtons.forEach((button) => {
   button.addEventListener("click", displayModal);
 });
-
 
 const closeModal = () => {
   modal.classList.remove("modal--show");
   form.classList.remove("modal__form--show");
   body.classList.remove("body__overflow");
-
-}
+};
 closeButton.addEventListener("click", closeModal);
 
+const radioButtons = document.querySelectorAll(".modal__radio-button");
 
+//display pledge input if radio button is checked
+const displayPledgeInput = () => {
+  radioButtons.forEach((radio) => {
+    if (radio.checked) {
+      radio.closest(".modal__reward").classList.add("checked");
+    } else {
+      radio.closest(".modal__reward").classList.remove("checked");
+    }
+  });
+};
+
+radioButtons.forEach((radio) => {
+  radio.addEventListener("click", displayPledgeInput);
+});
